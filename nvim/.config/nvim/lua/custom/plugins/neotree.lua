488beab9586d21202fe9abe34e29dc81812c0b3b
Nicolai Kobber <nkobber@gmail.com>
2024-04-09 13:16:21 +0200

    rename filetree.lua -> neotree.lua

---
+++ nvim/.config/nvim/lua/custom/plugins/neotree.lua
@@ -0,0 +1,25 @@
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+  },
+  opts = {
+    sources = { 'filesystem', 'buffers', 'git_status', 'document_symbols' },
+    open_files_do_not_replace_types = { 'terminal', 'Trouble', 'trouble', 'qf', 'Outline' },
+    filesystem = {
+      bind_to_cwd = false,
+      follow_current_file = { enabled = true },
+      use_libuv_file_watcher = true,
+    },
+  },
+  keys = {
+    {
+      '<leader>e',
+      '<cmd>Neotree focus<CR>',
+      desc = 'Neotree',
+    },
+  },
+}
