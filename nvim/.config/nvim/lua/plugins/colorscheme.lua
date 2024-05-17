626332056f3e8ce19d31880ef445425bd923f937
Nicolai Kobber <nkobber@gmail.com>
2024-05-18 00:48:23 +0200

    remove comment and colorscheme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,31 +1,15 @@
 return {
-  'scottmckendry/cyberdream.nvim',
-  lazy = false,
+  'folke/tokyonight.nvim',
   priority = 1000,
-  config = function()
-    require('cyberdream').setup {
-      -- Recommended - see "Configuring" below for more config options
-      transparent = true,
-      italic_comments = true,
-      hide_fillchars = true,
-      borderless_telescope = true,
-      terminal_colors = true,
-    }
-    vim.cmd 'colorscheme cyberdream' -- set the colorscheme
+  opts = {
+    transparent = true,
+    styles = {
+      sidebars = 'transparent',
+      floats = 'transparent',
+    },
+  },
+  init = function()
+    vim.cmd.colorscheme 'tokyonight-night'
+    vim.cmd.hi 'Comment gui=none'
   end,
 }
--- return {
---   'folke/tokyonight.nvim',
---   priority = 1000,
---   opts = {
---     transparent = true,
---     styles = {
---       sidebars = 'transparent',
---       floats = 'transparent',
---     },
---   },
---   init = function()
---     vim.cmd.colorscheme 'tokyonight-night'
---     vim.cmd.hi 'Comment gui=none'
---   end,
--- }
