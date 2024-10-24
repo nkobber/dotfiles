723e0da1e7dee355ee586423023f14061031954e
Nicolai Kobber <nkobber@gmail.com>
2024-10-24 09:56:11 +0200

    switch to rose-pine

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,16 @@
 return {
-  'scottmckendry/cyberdream.nvim',
-  lazy = false,
+  'rose-pine/neovim',
+  as = 'rose-pine',
   priority = 1000,
   init = function()
-    vim.cmd.colorscheme 'cyberdream'
-    -- vim.cmd.hi 'Comment gui=none'
+    vim.cmd.colorscheme 'rose-pine'
+    vim.cmd.hi 'Comment gui=none'
+  end,
+  config = function()
+    require('rose-pine').setup {
+      styles = {
+        transparency = true,
+      },
+    }
   end,
 }
