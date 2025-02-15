34dbb8aee88e09894e79d9bd58ce597d7a7c5719
Nicolai <nkobber@gmail.com>
2025-02-15 21:51:37 +0100

    switch to lazyvim

--- nvim/.config/nvim/lua/plugins/editor.lua
+++
@@ -1,7 +0,0 @@
-return {
-  -- Detect tabstop and shiftwidth automatically
-  'tpope/vim-sleuth',
-
-  -- Highlight todo, notes, etc in comments
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
-}
